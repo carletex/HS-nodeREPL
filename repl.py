cc9b4df45055ff697b7b4da6f33b13c1794e96eb
carletex <oceanrdn@gmail.com>
2014-08-26 15:20:26 -0400

    Basic behavior request-response

--- repl.py
+++ repl.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
+import urllib2
+import json
 import sys
-import os
 from hs_oauth import get_access_token, request
 
-# HS auth
+def process_cmd(cmd):
+	if cmd == 'exit()':
+		sys.exit()
+	response = request(access_token, HS_BASE_URL + cmd)
+	return json.dumps(response, indent=4)
+
 HS_BASE_URL = 'https://www.hackerschool.com/api/v1'
 access_token, refresh_token = get_access_token()
 
 print 'HS OAauth: access token received'
-print 'Listening to messages...'
+print 'Listening to commands. Type \'help\' for a list of commands'
+
+while True:
+	cmd = raw_input('HS> ')
+	print process_cmd(cmd)
